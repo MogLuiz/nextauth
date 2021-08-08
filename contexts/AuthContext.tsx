194abed0b5f8cf0aa16c49d56fc177ae253f07b1
Luiz Henrique <luizhjramos@outlook.com>
2021-08-07 22:32:55 -0300

    Contexto de autenticação

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode } from "react";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderChildren = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderChildren) {
+  const isAuthenticated = false;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    console.log({ email, password });
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
